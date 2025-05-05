64b589039c3bd84db7c770e25ed6d4271996242a
marvelpastor <marvelpastor@live.com>
2025-05-05 09:56:35 -0600

    added a way to read the file and print its contents to the terminal

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("greetings boots")
+""" 
+It takes a filepath as input 
+and returns the contents of the file 
+as a string.
+"""
+def get_book_text (path):
+    with open(path) as f:
+        text = f.read()
+    return text
+
+"""
+Uses get_book_text 
+with the relative path 
+to your frankenstein.txt file 
+to print the entire contents of the book 
+to the console.
+"""
+def main ():
+    print(get_book_text ("./books/frankenstein.txt"))
+    
+
+main()
